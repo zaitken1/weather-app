8ea913441ff681346f8fc56243755164170f9e3e
zaitken <zaraaitken18@gmail.com>
2022-12-18 17:48:37 +0000

    adds media queries to improve responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,32 @@
+//ADD CURRENT DATE TO CURRENT WEATHER ELEMENT
 //Get p element for current day
 var currentDateEl = $('.current-date');
 
 //Add current date
 var currentDay = moment().format('dddd Do MMMM YYYY');
 currentDateEl.text(currentDay);
+
+//GET HTML ELEMENTS
+var searchInput = $('.search-input');
+var searchBtn = $('.searchBtn');
+var itemWrapper = $('main');
+
+function getWeatherData(event) {
+    event.preventDefault();
+    var searchText = searchInput.val();
+
+    console.log(searchText);
+}
+
+function init() {
+    searchBtn.click(getWeatherData)
+}
+
+init()
+
+
+
+//Grab HTML elements
+//Get inputs value on search button click
+//Grab data related to user's search
+//Inject the weather items into the DOM based on user's search
---
+++ scripts/weatherdata.js
@@ -0,0 +1,37 @@
+var weatherData = [
+    {
+        date: '18/12/2022',
+        icon: 'https://www.pngitem.com/pimgs/m/264-2645717_sunny-sun-icon-svg-hd-png-download.png',
+        temperature: '32C',
+        wind: '3mph',
+        humidity: '80%'
+    },
+    {
+        date: '19/12/2022',
+        icon: 'https://www.pngitem.com/pimgs/m/264-2645717_sunny-sun-icon-svg-hd-png-download.png',
+        temperature: '24C',
+        wind: '12mph',
+        humidity: '85%'
+    },
+    {
+        date: '20/12/2022',
+        icon: 'https://www.pngitem.com/pimgs/m/264-2645717_sunny-sun-icon-svg-hd-png-download.png',
+        temperature: '19C',
+        wind: '10mph',
+        humidity: '84%'
+    },
+    {
+        date: '21/12/2022',
+        icon: 'https://www.pngitem.com/pimgs/m/264-2645717_sunny-sun-icon-svg-hd-png-download.png',
+        temperature: '20C',
+        wind: '7mph',
+        humidity: '89%'
+    },
+    {
+        date: '22/12/2022',
+        icon: 'https://www.pngitem.com/pimgs/m/264-2645717_sunny-sun-icon-svg-hd-png-download.png',
+        temperature: '29C',
+        wind: '5mph',
+        humidity: '85%'
+    },
+]
