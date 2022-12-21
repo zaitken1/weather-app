839ea4848f0aad8ca06d24b352840fb08e8c32dc
zaitken <zaraaitken18@gmail.com>
2022-12-21 15:03:56 +0000

    adds HTML for 5-day forecast using template literal string - unfinished

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,9 @@ var currentDay = moment().format('dddd Do MMMM YYYY');
 var searchInput = $('.search-input');
 var searchBtn = $('.searchBtn');
 var itemWrapper = $('main');
+var introPara = $('.introPara');
+var forecastSection = $('.forecast-section');
+var forecastEl = $('.forecast');
 
 // function displayMatches() {
 //     itemWrapper.html('');
@@ -33,21 +36,21 @@ var itemWrapper = $('main');
 //         </section >
 
 //         <h2 class="forecast-h2 row justify-center align-center">5-Day Forecast</h2>
-//         <section class="row">
-//             <div class="forecast">
-//                 <div class="weather-card column justify-center align-center">
-//                     <p class="future-date">18/12/2022</p>
-//                     <div class="card-content row">
-//                         <div class="column">
-//                             <i class="future-icon far fa-sun"></i>
-//                             <p class="future-temp">32 C</p>
-//                         </div>
-//                         <div class="column">
-//                             <p class="future-wind">Wind: 32mph</p>
-//                             <p class="future-humidity">Humidity: 80%</p>
-//                         </div>
-//                     </div>
+// <section class="row">
+//     <div class="forecast">
+//         <div class="weather-card column justify-center align-center">
+//             <p class="future-date">18/12/2022</p>
+//             <div class="card-content row">
+//                 <div class="column">
+//                     <i class="future-icon far fa-sun"></i>
+//                     <p class="future-temp">32 C</p>
 //                 </div>
+//                 <div class="column">
+//                     <p class="future-wind">Wind: 32mph</p>
+//                     <p class="future-humidity">Humidity: 80%</p>
+//                 </div>
+//             </div>
+//         </div>
 //                 <div class="weather-card column justify-center align-center">
 //                     <p class="future-date">18/12/2022</p>
 //                     <div class="card-content row">
@@ -106,17 +109,16 @@ var itemWrapper = $('main');
 // }
 // }
 
-//OPENWEATHER API
+//OPENWEATHER API LINKS
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key} - CURRENT WEATHER
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} - 5 DAY FORECAST
 
+//GLOBAL VARIABLES REQUIRED FOR OPENWEATHER API CALLS
 var apiKey = 'cc0b5674499c26e188072975d36326e1';
 var baseURL = 'https://api.openweathermap.org/data/2.5/';
 var currentURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
 var forecastURL = baseURL + `forecast?appid=${apiKey}&units=metric&`;
 var iconURL = 'https://openweathermap.org/img/w/';
-
-
 
 function getWeatherData(event) {
     event.preventDefault();
@@ -130,7 +132,6 @@ function getWeatherData(event) {
 
                     itemWrapper.html('');
 
-                    // for (var match of matches) {
                     itemWrapper.html(`
                         <section class="row">
                             <div class="current-weather align-center">
@@ -140,11 +141,11 @@ function getWeatherData(event) {
                                 </div>
                                 <div>
                                     <div class="row">
-                                        <i class="current-weather-icon far fa-sun"></i>
-                                        <p class="current-temp">${Math.round(currentData.main.temp)}</p>
+                                        <i class="current-weather-icon" href="${iconURL + currentData.weather[0].icon}.png"></i>
+                                        <p class="current-temp">${Math.round(currentData.main.temp)}°C</p>
                                     </div>
-                                    <p class="current-humidity">Humidity:${currentData.main.humidity}</p>
-                                    <p class="current-wind">Wind: ${currentData.wind.speed}</p>
+                                    <p class="current-humidity">Humidity: ${currentData.main.humidity}%</p>
+                                    <p class="current-wind">Wind: ${currentData.wind.speed}mph</p>
                                 </div>
                             </div>
                         </section >
@@ -152,8 +153,35 @@ function getWeatherData(event) {
 
                     $.get(forecastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`)
                         .then(function (forecastData) {
-                            for (var castObj of forecastData.list) {
-                                console.log(`${iconURL + castObj.weather[0].icon}.png`)
+
+                            
+
+
+                            for (var forecastObj of forecastData.list) {
+                                console.log(forecastData);
+                                console.log(forecastObj.dt_txt);
+                                console.log(`${iconURL + forecastObj.weather[0].icon}.png`);
+                                console.log(Math.round(forecastObj.main.temp));
+                                console.log(forecastObj.main.humidity);
+                                console.log(forecastObj.wind.speed);
+
+                                forecastSection.show();
+
+                                forecastEl.html(`
+                                        <div class="weather-card column justify-center align-center">
+                                            <p class="future-date">${forecastObj.dt_txt}</p>
+                                            <div class="card-content row">
+                                                <div class="column">
+                                                    <i class="future-icon" href="${iconURL + forecastObj.weather[0].icon}.png"></i>
+                                                    <p class="future-temp">${Math.round(forecastObj.main.temp)}°C</p>
+                                                </div>
+                                                <div class="column">
+                                                    <p class="future-wind">Wind: ${forecastObj.wind.speed}</p>
+                                                    <p class="future-humidity">Humidity: ${forecastObj.main.humidity}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                `)
                             }
                         });
 
